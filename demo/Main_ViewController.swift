de94c142dba5addb5ccebcd059221e878aadedbe
ninjachen <nyto9999@gmail.com>
2021-07-19 11:49:48 +0800

    coordinator

--- demo/Main_ViewController.swift
+++ demo/Main_ViewController.swift
@@ -9,14 +9,34 @@ import UIKit
 import FirebaseAuth
 class Main_ViewController: UIViewController {
     
+    
+    static func instantiate() -> Main_ViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: .main)
+        let controller = storyboard.instantiateViewController(identifier: "Main_ViewController") as! Main_ViewController
+        return controller
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        validate_auth()
+        setup()
         
     }
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        validate_auth()
+    }
+    
+    @objc private func tapped_right_barbutton(){
+        print("yes")
+    }
+    
+    private func setup(){
+        let plus_image = UIImage(systemName: "plus.circle.fill")
+        let bar_buttonitem = UIBarButtonItem(image: plus_image, style: .plain, target: self, action: #selector(tapped_right_barbutton))
+        bar_buttonitem.tintColor = .primary
+        navigationItem.rightBarButtonItem = bar_buttonitem
+        navigationItem.title = "Main"
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     private func validate_auth(){
---
+++ demo/UIColor + Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor + Extension.swift
+//  demo
+//
+//  Created by 宇宣 Chen on 2021/7/19.
+//
+
+import UIKit
+
+extension UIColor {
+    static let primary = UIColor(red: 0/255, green: 39/255, blue: 255/255, alpha: 0.6)
+}
